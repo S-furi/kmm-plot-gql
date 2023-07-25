9810712e87703208a48bc88d4f6feb756ed620c1
S-furi <stefano.furi@studio.unibo.it>
2023-07-25 19:19:34 +0200

    fix: fixed client applicatio startup.
    Still, application is not working due to wrong connection to server and
    the subscription endpoint.

--- src/jsMain/kotlin/PlotApp.kt
+++ src/jsMain/kotlin/PlotApp.kt
@@ -1,3 +1,59 @@
-import client.DefaultGraphQLClient
 
-private val apolloClient = DefaultGraphQLClient.Builder().serverUrl().build()
+import client.DefaultGraphQLClient
+import com.apollographql.apollo3.api.Optional
+import gql.client.PointsSubscription
+import kotlinx.browser.document
+import kotlinx.browser.window
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.dom.clear
+import org.jetbrains.letsPlot.Figure
+import org.jetbrains.letsPlot.frontend.JsFrontendUtil
+import org.jetbrains.letsPlot.geom.geomPoint
+import org.jetbrains.letsPlot.geom.geomSmooth
+import org.jetbrains.letsPlot.letsPlot
+
+private val apolloClient = DefaultGraphQLClient.Builder().serverUrl(port = 8082).build().client
+
+fun main() {
+    window.onload = { createContext() }
+}
+
+fun createContext() {
+    val data = mapOf("x" to mutableListOf<Int>(), "y" to mutableListOf())
+
+    MainScope().launch {
+        getData()
+            .onEach {
+                it.data?.randomPoints?.let { point ->
+                    data["x"]?.add(point.x)
+                    data["y"]?.add(point.y)
+                }
+                println(it.data?.randomPoints)
+                addPlotToDiv(getPlot(data))
+            }
+            .onCompletion {
+                println("Done Collecting")
+                val finalPlot = getPlot(data) + geomSmooth(deg = 3)
+                addPlotToDiv(finalPlot)
+            }
+            .collect()
+    }
+}
+
+fun addPlotToDiv(p: Figure) {
+    val contentDiv = document.getElementById("content")
+    contentDiv?.clear()
+    contentDiv?.appendChild(JsFrontendUtil.createPlotDiv(p))
+}
+
+fun getPlot(data: Map<String, Any>, xx: String = "x", yy: String = "y") =
+    letsPlot(data) {
+        x = xx
+        y = yy
+    } + geomPoint(color = "red", size = 4.0)
+
+fun getData() = apolloClient.subscription(PointsSubscription(Optional.present(10))).toFlow()
